d6cbcca812ee79e6fd1e65769404afac7fb4d86b
Divyansh Kushwaha <thedroiddiv@gmail.com>
2024-05-13 11:45:51 +0530

    chore: add sample test case

---
+++ src/test/kotlin/com/thedroiddiv/klibrosa/SampleTest.kt
@@ -0,0 +1,12 @@
+package com.thedroiddiv.klibrosa
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+
+    @Test
+    fun testAddition() {
+        assertEquals(4, 2 + 2)
+    }
+}
