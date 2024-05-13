f9e794810ecec8545a14408d97e33c17d24dc91c
Divyansh Kushwaha <thedroiddiv@gmail.com>
2024-05-13 12:32:41 +0530

    chore: convert FileFormatNotSupportedException for Kotlin

---
+++ src/main/java/com/thedroiddiv/klibrosa/exception/FileFormatNotSupportedException.kt
@@ -0,0 +1,10 @@
+package com.thedroiddiv.klibrosa.exception
+
+
+/**
+ *
+ * This Class is an custom exception class to throw exceptions when unsupported files are provided as input for processing.
+ *
+ * @author vvasanth
+ */
+class FileFormatNotSupportedException(message: String?) : Exception(message)
